66933e7257ea8be424fc3bfa01d77d01551184d2
NaTT <1103625169lixuan@gmail.com>
2025-02-03 17:14:26 +1100

    upload script and update manual

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Automate-HTML-CSS-JS-Script
-A script to generate three custom names files with suffix .html, .css, .js
+A script to generate a directory with custom name and three files 'index.html', 'app.js', and 'styles.css'. There is a default template at index.html as well.
+
+## Prerequisites
+
+- Ensure you have Node.js installed on your system.
+- If not yet, [Link to Download Node JS](https://nodejs.org/en/download)
+
+## Manual
+
+1. **Open Terminal:**
+   - Navigate to the directory where your script is located.
+
+2. **Run the Script:**
+   - Use the following command to run the script:
+    ```bash
+    node yourScriptName.js [folderName]
+    ```
+   - Replace `yourScriptName.js` with the actual name of your script file.
+   - `[folderName]` is optional. If not provided, the default name `Project` will be used.
+
+3. **Directory Handling:**
+   - If a directory with the specified name already exists, the script will prompt:
+    ```
+    Directory [folderName] already exists. Type YES to replace it, Type NO to terminate:
+    ```
+   - Type `YES` to delete the existing directory and create a new one.
+   - Type `NO` to terminate the operation without making any changes.
+
+4. **Files Created:**
+   - `index.html`: Contains default HTML content.
+   - `app.js`: An empty JavaScript file.
+   - `styles.css`: An empty CSS file.
---
+++ script.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const folderName = process.argv[2] || 'Project';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+if (fs.existsSync(folderName)) {
+    rl.question(`Directory ${folderName} already existed, type YES to replace, NO to terminate: `, (answer) => {
+        if (answer.toUpperCase() == 'YES') {
+            fs.rmSync(folderName, {recursive: true});
+            createDir();
+        } else if (answer.toUpperCase() == 'NO') {
+            console.log('Terminated');
+            rl.close();
+        }
+    })
+} else {
+    createDir();
+}
+
+function createDir() {
+    try {
+        fs.mkdirSync(folderName);
+        fs.writeFileSync(path.join(folderName, 'index.html'), defaultHTML());
+        fs.writeFileSync(path.join(folderName, 'app.js'), '');
+        fs.writeFileSync(path.join(folderName, 'styles.css'), '');
+    
+        console.log(`Directory ${folderName} created successfully`);
+    } catch (e) {
+        console.log("CATCH ERROR: ");
+        console.log(e);
+    } finally {
+        rl.close();
+    }
+}
+
+function defaultHTML() {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="app.js"></script>
+</body>
+</html>
+    `;
+}
+
